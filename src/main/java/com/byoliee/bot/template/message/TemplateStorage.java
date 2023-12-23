0814babcaf261ede093887fbf7b70f2d86c892a3
Mikhail <mu.korepanov@gmail.com>
2023-12-24 01:39:47 +0400

    try 1

--- src/main/java/com/byoliee/bot/template/message/TemplateStorage.java
+++ src/main/java/com/byoliee/bot/template/message/TemplateStorage.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,8 +25,7 @@ public class TemplateStorage {
 
     public String getTemplate(String templateName) {
         try {
-            ClassLoader classLoader = getClass().getClassLoader();
-            Resource resource = new ClassPathResource(templatesDir + "/" + templateName, classLoader);
+            Resource resource = resourceLoader.getResource("classpath:" + templatesDir + File.separator + templateName);
             Path path = resource.getFile().toPath();
             return Files.readString(path);
         } catch (IOException e) {
